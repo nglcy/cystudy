3cb5f8a288346951de5daf4a5823e7263f43322a
chenyuan190622@163.com <chenyuan190622@163.com>
2018-11-25 17:55:58 +0800

    push the new code to My branch

---
+++ src/main/java/cy/study/po/Person.java
@@ -0,0 +1,65 @@
+package cy.study.po;
+
+import java.util.Date;
+
+/**
+ * @author chenyuan
+ * @date 2018-11-25 17:45
+ */
+public class Person {
+    private int id;
+    private String name;
+    private String address;
+    private int status;
+    private Date createTime;
+    private Date modifytime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getModifytime() {
+        return modifytime;
+    }
+
+    public void setModifytime(Date modifytime) {
+        this.modifytime = modifytime;
+    }
+}
+
