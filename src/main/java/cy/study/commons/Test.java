dea3dce48993f9c19390ad05e4c78cd7fe20ed04
chenyuan190622@163.com <chenyuan190622@163.com>
2018-11-25 19:15:41 +0800

    add test

---
+++ src/main/java/cy/study/commons/Test.java
@@ -0,0 +1,21 @@
+package cy.study.commons;
+
+
+
+/**
+ * @author chenyuan
+ * @date 2018-11-25 19:10
+ */
+public class Test{
+    public static void main(String[] args) {
+
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "");
+        java.lang.String str = new String();
+        if (!str.isEmpty()) {
+            System.out.println("nihaoshijie");
+        }
+    }
+
+
+}
