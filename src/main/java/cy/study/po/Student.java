f4ab0c5d44924640fdc274770ee2f082d9f4224a
chenyuan190622@163.com <chenyuan190622@163.com>
2018-11-25 17:20:10 +0800

    frist  commit

---
+++ src/main/java/cy/study/po/Student.java
@@ -0,0 +1,55 @@
+package cy.study.po;
+
+import java.util.Date;
+
+/**
+ * @author chenyuan
+ * @date 2018-11-25 16:59
+ */
+public class Student {
+    private int id ;
+     private  String name;
+     private String password ;
+     private Date createTime;
+     private Date modifyTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+}
---
+++ src/main/java/cy/study/po/Tee.java
@@ -0,0 +1,13 @@
+package cy.study.po;
+
+/**
+ * @author chenyuan
+ * @date 2018-11-25 17:00
+ */
+public class Tee {
+
+    public void first(){
+        System.out.println("第一get               二恶   潜入     345 你好 捏的 " +
+                "啊 ");
+    }
+}
